6e43afa3b9e795e89436ad6a3cf419a555a52611
Michael Koontz <mikoontz@gmail.com>
2022-12-28 15:27:24 -0700

    include max_neighbors to the list; ultimately I think it'd be ideal for a user to be able to create the new config files from a data.frame like the one in this .R script, rather than generating the derived.yml file by hand too

--- workflow/01_determine-metashape-parameterization.R
+++ workflow/01_determine-metashape-parameterization.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(here)
+library(tidyr)
 
 am_dir <- file.path("~", "dev", "ofo", "automate-metashape")
 yaml_path <- here::here("configs")
@@ -9,21 +10,35 @@ metashape_path <- file.path(am_dir, "python", "metashape_workflow.py")
 young2022 <-
   tibble::tribble(~id, ~photo_alignment_qual, ~photo_downscale, 
                   ~dense_cloud_qual, ~cloud_downscale, ~depth_filtering,
-                  7,  "Low",    4, "Medium", 4, "Mild",
-                  8,  "Low",    4, "Medium", 4, "Moderate", 
-                  9,  "Medium", 2, "Medium", 4, "Mild",
-                  10, "Medium", 2, "Medium", 4, "Moderate", 
-                  11, "High",   1, "Medium", 4, "Mild",
-                  12, "High",   1, "Medium", 4, "Moderate", 
-                  13, "Low",    4, "High",   2, "Mild",
-                  14, "Low",    4, "High",   2, "Moderate", 
-                  15, "Medium", 2, "High",   2, "Mild",
-                  16, "Medium", 2, "High",   2, "Moderate", 
-                  17, "High",   1, "High",   2, "Mild",
-                  18, "High",   1, "High",   2, "Moderate")
+                  7,  "low",    4, "medium", 4, "mild",
+                  8,  "low",    4, "medium", 4, "moderate", 
+                  9,  "medium", 2, "medium", 4, "mild",
+                  10, "medium", 2, "medium", 4, "moderate", 
+                  11, "high",   1, "medium", 4, "mild",
+                  12, "high",   1, "medium", 4, "moderate", 
+                  13, "low",    4, "high",   2, "mild",
+                  14, "low",    4, "high",   2, "moderate", 
+                  15, "medium", 2, "high",   2, "mild",
+                  16, "medium", 2, "high",   2, "moderate", 
+                  17, "high",   1, "high",   2, "mild",
+                  18, "high",   1, "high",   2, "moderate")
 
 # For each of these combinations, we also added three different values of the
 # BuildDenseCloud "max_neighbors" parameter
+
+metashape_version_effect_recipe <-
+  tidyr::expand_grid(young2022, data.frame(max_neighbors = c(100, 50, 150))) %>% 
+  dplyr::mutate(id = dplyr::case_when(max_neighbors == 50 ~ paste0(sprintf("%02d", id), "a"),
+                                      max_neighbors == 100 ~ paste0(sprintf("%02d", id), "b"),
+                                      max_neighbors == 150 ~ paste0(sprintf("%02d", id), "c"))) %>% 
+  dplyr::mutate(run_name = paste("metashape-version-effect_config", 
+                                 id, 
+                                 photo_downscale,
+                                 cloud_downscale,
+                                 depth_filtering,
+                                 max_neighbors,
+                                 sep = "_"))
+
 system2(command = "Rscript", args = paste(file.path(am_dir, "R", "prep_configs.R"), 
                                           here::here("configs"),
                                           metashape_path))
