a685fdf1efa6d4c2cc57ec8d986ae9aac11d4072
Derek Young <djyoung@ucdavis.edu>
2023-01-16 20:31:17 +0000

    Detection with explicit vwf, more ranges, radius floor, height floor

--- workflow/detect-trees.R
+++ workflow/detect-trees.R
@@ -16,26 +16,43 @@ source(here("workflow/convenience-functions.R"))
 ### CONSTANTS ###
 
 # Maximum number of detected trees that is at all reasonable. If more than this are detected, the results will not be saved (to save on file storage)
-MAX_TREE_COUNT = 50000
+MAX_TREE_COUNT = 10000
 
 ## Specify the different sets of VWF parameters as fully factorial combo of the following parameters
 # For lidR LMF
-intercepts = seq(0, 2.5, by = 0.25)
-slopes = seq(0,0.1, by = 0.01)
-window_mins = 0.12
+method = "lmf"
+intercepts = seq(0, 2.5, by = 0.5)
+slopes = seq(0,0.12, by = 0.02)
+window_mins = c(0.5, 1, 2)
 window_maxs = 100
 smooths = c(0, 3, 7, 11)
 
-# For ForestTools VWF (a negative slope is the cue that it's supposed to be run through VWF)
-ft_intercepts = seq(0, 0.4, by = 0.1)
-ft_slopes = seq(-0.01, -0.05, by = -0.01)
-ft_window_mins = 0.12
-ft_window_maxs = 100
+# A lidR LMF set with negative intercepts
+neg_method = "lmf"
+neg_intercepts = seq(0, -1, by = -0.5)
+neg_slopes = seq(0.05,0.14, by = 0.02)
+neg_window_mins = c(0.5, 1, 2)
+neg_window_maxs = 100
+neg_smooths = c(0, 3, 7, 11)
+
+# For ForestTools VWF (a negative slope is the cue that it's supposed to be run through VWF, but in the actual window function it's treated as a postiive slope)
+ft_method = "vwf"
+ft_intercepts = seq(0, 0.4, by = 0.2)
+ft_slopes = seq(0.02, 0.06, by = 0.02)
+ft_window_mins = 0
+ft_window_maxs = 0
 ft_smooths = c(0, 3, 7, 11)
 
+# A VWF set with negative intercepts
+neg_ft_method = "vwf"
+neg_ft_intercepts = seq(0, -1, by = -0.5)
+neg_ft_slopes = seq(0.03,0.08, by = 0.02)
+neg_ft_window_mins = 0
+neg_ft_window_maxs = 0
+neg_ft_smooths = c(0, 3, 7, 11)
 
 
-# # Param ranges for initial search:
+# # Param ranges used for initial search:
 # intercepts = seq(0, 4, by = 0.5)
 # slopes = seq(0,0.2, by = 0.02)
 # window_mins = 0.12
@@ -43,7 +60,7 @@ ft_smooths = c(0, 3, 7, 11)
 # smooths = c(0, 11)
 
 ## Specify output dir
-ttops_dir = datadir("meta200/drone/L3/ttops_fullrun01/")
+ttops_dir = datadir("meta200/drone/L3/ttops_fullrun02/")
 
 ## Get the file listing of the output dir (to determine if a file we aim to produce already exists)
 ttops_dir_contents = list.files(ttops_dir, full.names=TRUE)
@@ -72,10 +89,10 @@ pad_3dec = function(x) {
 ### Busines logic functions ###
 
 # Function to make a vwf with a specified slope and intercept
-make_vwf <- function(intercept, slope, window_min, window_max) { 
+make_vwf <- function(method, intercept, slope, window_min, window_max) { 
   vwf = function(x) {
-    y = intercept + abs(slope)*x # abs is here because a negative slope is a flag indicating should run ForestTools::vwf instead of lidR::lmf
-    if(slope >= 0) { # this is a check to see if the slope is negative, as a flag indicating should run ForestTools::vwf instead, and if so, don't set floor or ceiling
+    y = intercept + slope*x 
+    if(method == "lmf") { # When running ForestTools::vwf, don't set floor or ceiling
       y = pmax(y, window_min, na.rm=TRUE) # for lidR::lmf, can't go smaller than chm resolution (chm resolution is 0.12 m)
       y = pmin(y, window_max, na.rm=TRUE)
     }
@@ -123,7 +140,7 @@ itd_onechm_allvwfs = function(chm_file) {
   itd_onevwfparamset = function(focal_vwfparams) {
     
     # Filename / path to save to
-    filename = paste0("ttops_", chm_name, "_", focal_vwfparams$intercept |> pad_3dec(), "_", focal_vwfparams$slope |> pad_3dec(), "_", focal_vwfparams$window_min |> pad_3dec(), "_", focal_vwfparams$window_max |> pad_3dec(), "_", focal_vwfparams$smooth |> pad_2dig(), ".gpkg")
+    filename = paste0("ttops_", chm_name, "_", focal_vwfparams$method, "_", focal_vwfparams$intercept |> pad_3dec(), "_", focal_vwfparams$slope |> pad_3dec(), "_", focal_vwfparams$window_min |> pad_3dec(), "_", focal_vwfparams$window_max |> pad_3dec(), "_", focal_vwfparams$smooth |> pad_2dig(), ".gpkg")
     file_path = paste0(ttops_dir, filename)
     filepath_placeholder = str_replace(file_path,".gpkg$", ".txt_placeholder") # this is used instead of writing a gpkg when the tree detection results are unrealistic. save this placeholder so we don't have to run the ITD again when rerunning
     
@@ -133,15 +150,15 @@ itd_onechm_allvwfs = function(chm_file) {
       return(TRUE)
     }
     
-    variable_window_function = make_vwf(intercept = focal_vwfparams$intercept, slope = focal_vwfparams$slope, window_min = focal_vwfparams$window_min, window_max = focal_vwfparams$window_max)
+    variable_window_function = make_vwf(method = focal_vwfparams$method, intercept = focal_vwfparams$intercept, slope = focal_vwfparams$slope, window_min = focal_vwfparams$window_min, window_max = focal_vwfparams$window_max)
     
     ## load the right CHM
     chm_foc = chm_smoothed[[as.character(focal_vwfparams$smooth)]]
     
     ## if the slope is positive, that is the flag that we should run lidR::lmf
-    if(focal_vwfparams$slope >= 0) {
-      ttops = lidR::locate_trees(chm_foc, lmf(variable_window_function, shape="circular"))
-    } else {
+    if(focal_vwfparams$method == "lmf") {
+      ttops = lidR::locate_trees(chm_foc, lmf(variable_window_function, shape="circular", hmin = 5))
+    } else if(focal_vwfparams$method == "vwf")  {
     ## if the slope is negative, that is the flag that we should run ForestTools::vwf
       chm_foc = raster::raster(chm_foc)
       ttops = ForestTools::vwf(CHM = chm_foc, winFun = variable_window_function, minHeight = 5, maxWinDiameter = 199)
@@ -150,7 +167,7 @@ itd_onechm_allvwfs = function(chm_file) {
     
     # if there are too many ttops, don't save them, but write a placeholder file so we don't try again
     if(nrow(ttops) > MAX_TREE_COUNT) {
-      write("Dummy placeholder because ttops predicted were unrealistic so not worth saving the file.", file = filepath_placeholder)
+      write(paste0("Dummy placeholder because ttops predicted were unrealistic so not worth saving the file. Ttops detected: ", nrow(ttops)), file = filepath_placeholder)
       return(FALSE)
     }
     
@@ -169,9 +186,12 @@ itd_onechm_allvwfs = function(chm_file) {
 if(!dir.exists(ttops_dir)) dir.create(ttops_dir, recursive=TRUE)
 
 # Create factorial combo of vwf params
-vwfparams_lidr = expand.grid(intercept = intercepts,slope = slopes, window_min = window_mins, window_max = window_maxs, smooth = smooths)
-vwfparams_foresttools = expand.grid(intercept = ft_intercepts,slope = ft_slopes, window_min = ft_window_mins, window_max = ft_window_maxs, smooth = ft_smooths)
-vwfparams = bind_rows(vwfparams_lidr, vwfparams_foresttools)
+vwfparams_lidr = expand.grid(method = method, intercept = intercepts,slope = slopes, window_min = window_mins, window_max = window_maxs, smooth = smooths)
+vwfparams_lidr_negintercept = expand.grid(method = method, intercept = neg_intercepts,slope = neg_slopes, window_min = neg_window_mins, window_max = neg_window_maxs, smooth = neg_smooths)
+vwfparams_foresttools = expand.grid(method = method, intercept = ft_intercepts,slope = ft_slopes, window_min = ft_window_mins, window_max = ft_window_maxs, smooth = ft_smooths)
+vwfparams_foresttools_negintercept = expand.grid(method = method, intercept = neg_ft_intercepts,slope = neg_ft_slopes, window_min = neg_ft_window_mins, window_max = neg_ft_window_maxs, smooth = neg_ft_smooths)
+vwfparams = bind_rows(vwfparams_lidr, vwfparams_lidr_negintercept, vwfparams_foresttools, vwfparams_foresttools_negintercept)
+vwfparams$id = 1:nrow(vwfparams)
 
 vwfparams = vwfparams[!(vwfparams$intercept == 0 & vwfparams$slope == 0),] # remove ones with intercept and slope both == 0
 
@@ -185,4 +205,4 @@ set_lidr_threads(1) # Set to 1 because we're going to parallelize across ITD run
 # Run in parallel
 furrr_options(scheduling = Inf)
 plan(multisession)
-future_walk(chm_files_list,itd_onechm_allvwfs)
+future_walk(chm_files_list[33:48],itd_onechm_allvwfs)
