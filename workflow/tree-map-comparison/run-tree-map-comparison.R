cf08899ed4d195d33cb402217af23a0399db92cc
Derek Young <djyoung@ucdavis.edu>
2023-01-28 06:58:36 +0000

    Cosntant for buffer dist for clarity

--- workflow/tree-map-comparison/run-tree-map-comparison.R
+++ workflow/tree-map-comparison/run-tree-map-comparison.R
@@ -28,10 +28,12 @@ plot_bound_filepath =  datadir("study-area-perimeter/ground_map_mask_precise.geo
 
 # Location of temp directory (holds intermediate files between the comparison steps) and the directory for comparison outputs
 tmp_dir = datadir("temp")
-output_dir = datadir("meta200/itd-evals/itd-eval-dpf-ttops-run01")
+output_dir = datadir("meta200/itd-evals/itd-eval-fullrun02v2")
 
 # Location of the predicted tree maps to evaluate
-predicted_trees_path = datadir("meta200/drone/L3/dpf-ttops_run01/")
+predicted_trees_path = datadir("meta200/drone/L3/ttops_fullrun02/")
+
+INTERNAL_BUFFER_DIST = 10 # By how many meters to buffer in from the plot edge when computing precision and recall to ensure all predicted trees have a fair chance to match to an observed tree and vice-versa
 
 # Maximum number of predicted trees, beyond which consider it an extremely poor tree detection and skip it
 MAX_PREDICTED_TREES = 50000
@@ -43,7 +45,7 @@ MAX_PREDICTED_TREES = 50000
 # This function saves the prepared tree dataset back to its original filename
 prep_observed_tree_map_for_comparison(observed_trees_filepath = observed_trees_filepath,
                                       plot_bound_filepath =  plot_bound_filepath,
-                                      internal_plot_buffer_dist = 5) # By how many meters to buffer in from the plot edge when computing precision and recall to ensure all predicted trees have a fair chance to match to an observed tree
+                                      internal_plot_buffer_dist = INTERNAL_BUFFER_DIST) # By how many meters to buffer in from the plot edge when computing precision and recall to ensure all predicted trees have a fair chance to match to an observed tree
 
 
 # Open the observed trees to see how many there are overall and in the internal area (used to exclude predicted tree sets if there are many more predicted than observed trees)
@@ -56,102 +58,102 @@ observed_trees_internal = observed_trees[observed_trees$internal_area == TRUE, ]
 
 eval_one_predicted_set = function(predicted_trees_filepath) {
   
-    cat(" Evaluating", predicted_trees_filepath, "*******\n                                  ")
+  cat(" Evaluating", predicted_trees_filepath, "*******\n                                  ")
   
-    ## Check if already exists
-    predicted_tree_dataset_name = tools::file_path_sans_ext(basename(predicted_trees_filepath))
-    output_filename = paste0("stats_",predicted_tree_dataset_name,".csv")
-    # Create alternate filename that is used as a placholeder when we don't actually process the dataset, to mark that we condiered processing it and decided to skip it (so when we rerun, we don't try again)
-    output_filename_placeholder = paste0("stats_",predicted_tree_dataset_name,".placeholder_txt")
-    output_filepath = paste0(output_dir, "/tree_detection_evals/", output_filename)
-    output_filepath_placeholder = paste0(output_dir, "/tree_detection_evals/", output_filename_placeholder)
-
-    if(file.exists(output_filepath) | file.exists(output_filepath_placeholder)) {
-      cat("Stats file already exists:", output_filepath, ", skipping.\n")
-      return(TRUE)
-    }
-
-    
-    ### Check for a reasonable number of predicted trees and skip if unreasonable
-    
-    ## How many predicted trees are there? Reject sets where there are way too many
-    predicted_trees = st_read(predicted_trees_filepath)
-    
-    # If there is > MAX_PREDICTED_TREES, it's an unrealistic tree detection; skip
-    if(nrow(predicted_trees) > MAX_PREDICTED_TREES) {
-      cat("@@@@@@ Over", MAX_PREDICTED_TREES, "predicted trees. Skipping. @@@@@@\n")
-      write_file("Placeholder text", output_filepath_placeholder)
-      return(FALSE)
-    }
-
-    # Prepare the predicted tree dataset for comparison by adding the necessary attributes (including: whether in internally buffered area, which predicted tree the observed tree is matched to) and projecting the predicted tree dataset to the observed tree dataset
-    # This function saves the prepared tree dataset back to its original filename
-    prep_predicted_tree_map_for_comparison(predicted_trees_filepath = predicted_trees_filepath,
-                                  plot_bound_filepath =  plot_bound_filepath,
-                                  internal_plot_buffer_dist = 5) # By how many meters to buffer in from the plot edge when computing precision and recall to ensure all predicted trees have a fair chance to match to an observed tree
-    
-    
-    ### Check for a reasonable number of predicted trees and skip if unreasonable
-    
-    ## How many predicted trees are there? Reject sets where there are way too many
-    predicted_trees = st_read(predicted_trees_filepath)
-    predicted_trees_internal = predicted_trees[predicted_trees$internal_area == TRUE, ]
-    
-    # How many times more predicted trees then observed trees are there?
-    overprediction_factor = nrow(predicted_trees_internal) / nrow(observed_trees_internal)
-    
-    # If overpredicting by a factor of 8 or more, skip (trying to compute accuracy for huge ttop datasets is very slow)
-    if(overprediction_factor > 8) {
-      cat("@@@@@@ Too many trees predicted (overprediction factor: ", overprediction_factor, "). Skipping. @@@@@@\n")
-      write_file("Placeholder text", output_filepath_placeholder)
-      return(FALSE)
-    }
-    
-    # If underpredicting by a factor of 0.05 or smaller, skip (functions don't work when there are no trees)
-    if(overprediction_factor < 0.05) {
-      cat("@@@@@@ Too few trees predicted (overprediction factor: ", overprediction_factor, "). Skipping. @@@@@@\n")
-      write_file("Placeholder text", output_filepath_placeholder)
-      return(FALSE)
-    }
-    
-    
-    # This function saves (in tmp_dir) a gpkg of the observed trees, with a column indicating which predicted tree (if any) it was matched to
-    match_trees(observed_trees_filepath = observed_trees_filepath,
-                predicted_trees_filepath = predicted_trees_filepath,
-                tmp_dir = tmp_dir,
-                search_height_proportion = 0.5, # Within what fraction (+ or -) of the observed tree height is a predicted tree allowed to match
-                additional_overstory_comparison = TRUE, # In addition to comparing against *all* observed trees, should we compare against *overstory trees only*. If so, the observed trees stem map file needs to have an attribute "under_neighbor" previously assigned by the script scripts/ground_stem_map_assign_under_neighbor.R
-                search_distance_fun_slope = 0.1, # The slope of the linear function for relating observed tree height to the potential matching distance
-                search_distance_fun_intercept = 1)  # The intercept of the linear function for relating observed tree height to the potential matching distance
-    
-    # This function saves individual tree detection accuracy statistics (sensitivity, precision, F-score, etc) in tmp_dir
-    tree_det_stats(predicted_trees_filepath = predicted_trees_filepath,
+  ## Check if already exists
+  predicted_tree_dataset_name = tools::file_path_sans_ext(basename(predicted_trees_filepath))
+  output_filename = paste0("stats_",predicted_tree_dataset_name,".csv")
+  # Create alternate filename that is used as a placholeder when we don't actually process the dataset, to mark that we condiered processing it and decided to skip it (so when we rerun, we don't try again)
+  output_filename_placeholder = paste0("stats_",predicted_tree_dataset_name,".placeholder_txt")
+  output_filepath = paste0(output_dir, "/tree_detection_evals/", output_filename)
+  output_filepath_placeholder = paste0(output_dir, "/tree_detection_evals/", output_filename_placeholder)
+  
+  if(file.exists(output_filepath) | file.exists(output_filepath_placeholder)) {
+    cat("Stats file already exists:", output_filepath, ", skipping.\n")
+    return(TRUE)
+  }
+  
+  
+  ### Check for a reasonable number of predicted trees and skip if unreasonable
+  
+  ## How many predicted trees are there? Reject sets where there are way too many
+  predicted_trees = st_read(predicted_trees_filepath)
+  
+  # If there is > MAX_PREDICTED_TREES, it's an unrealistic tree detection; skip
+  if(nrow(predicted_trees) > MAX_PREDICTED_TREES) {
+    cat("@@@@@@ Over", MAX_PREDICTED_TREES, "predicted trees. Skipping. @@@@@@\n")
+    write_file("Placeholder text", output_filepath_placeholder)
+    return(FALSE)
+  }
+  
+  # Prepare the predicted tree dataset for comparison by adding the necessary attributes (including: whether in internally buffered area, which predicted tree the observed tree is matched to) and projecting the predicted tree dataset to the observed tree dataset
+  # This function saves the prepared tree dataset back to its original filename
+  prep_predicted_tree_map_for_comparison(predicted_trees_filepath = predicted_trees_filepath,
+                                         plot_bound_filepath =  plot_bound_filepath,
+                                         internal_plot_buffer_dist = INTERNAL_BUFFER_DIST) # By how many meters to buffer in from the plot edge when computing precision and recall to ensure all predicted trees have a fair chance to match to an observed tree
+  
+  
+  ### Check for a reasonable number of predicted trees and skip if unreasonable
+  
+  ## How many predicted trees are there? Reject sets where there are way too many
+  predicted_trees = st_read(predicted_trees_filepath)
+  predicted_trees_internal = predicted_trees[predicted_trees$internal_area == TRUE, ]
+  
+  # How many times more predicted trees then observed trees are there?
+  overprediction_factor = nrow(predicted_trees_internal) / nrow(observed_trees_internal)
+  
+  # If overpredicting by a factor of 8 or more, skip (trying to compute accuracy for huge ttop datasets is very slow)
+  if(overprediction_factor > 8) {
+    cat("@@@@@@ Too many trees predicted (overprediction factor: ", overprediction_factor, "). Skipping. @@@@@@\n")
+    write_file("Placeholder text", output_filepath_placeholder)
+    return(FALSE)
+  }
+  
+  # If underpredicting by a factor of 0.05 or smaller, skip (functions don't work when there are no trees)
+  if(overprediction_factor < 0.05) {
+    cat("@@@@@@ Too few trees predicted (overprediction factor: ", overprediction_factor, "). Skipping. @@@@@@\n")
+    write_file("Placeholder text", output_filepath_placeholder)
+    return(FALSE)
+  }
+  
+  
+  # This function saves (in tmp_dir) a gpkg of the observed trees, with a column indicating which predicted tree (if any) it was matched to
+  match_trees(observed_trees_filepath = observed_trees_filepath,
+              predicted_trees_filepath = predicted_trees_filepath,
+              tmp_dir = tmp_dir,
+              search_height_proportion = 0.5, # Within what fraction (+ or -) of the observed tree height is a predicted tree allowed to match
+              additional_overstory_comparison = TRUE, # In addition to comparing against *all* observed trees, should we compare against *overstory trees only*. If so, the observed trees stem map file needs to have an attribute "under_neighbor" previously assigned by the script scripts/ground_stem_map_assign_under_neighbor.R
+              search_distance_fun_slope = 0.1, # The slope of the linear function for relating observed tree height to the potential matching distance
+              search_distance_fun_intercept = 1)  # The intercept of the linear function for relating observed tree height to the potential matching distance
+  
+  # This function saves individual tree detection accuracy statistics (sensitivity, precision, F-score, etc) in tmp_dir
+  tree_det_stats(predicted_trees_filepath = predicted_trees_filepath,
+                 tmp_dir = tmp_dir,
+                 output_dir = output_dir)
+  
+  # This function saves area-based accuracy statistics in tmp_dir
+  area_based_stats(observed_trees_filepath = observed_trees_filepath,
+                   predicted_trees_filepath = predicted_trees_filepath,
+                   focal_region_polygon_filepath = plot_bound_filepath,
                    tmp_dir = tmp_dir,
-                   output_dir = output_dir)
-    
-    # This function saves area-based accuracy statistics in tmp_dir
-    area_based_stats(observed_trees_filepath = observed_trees_filepath,
-                     predicted_trees_filepath = predicted_trees_filepath,
-                     focal_region_polygon_filepath = plot_bound_filepath,
-                     tmp_dir = tmp_dir,
-                     virtual_plot_size = 30, # The height and width of contiguous square grid cells laid over the field plot for computing area-based statistics
-                     additional_overstory_comparison = TRUE) # In addition to comparing against *all* observed trees, should we compare against *overstory trees only*. If so, the observed trees stem map file needs to have an attribute "under_neighbor" previously assigned by the script scripts/ground_stem_map_assign_under_neighbor.R
-                     
-    # This function combines the individual tree detection and area-based accuracy statistics and saves in output_dir/tree_detection_evals
-    combine_stats(tmp_dir = tmp_dir,
-                  output_dir = output_dir,
-                  predicted_trees_filepath = predicted_trees_filepath)
-    
-    # This function compares the heights of paired predicted and observed trees and makes a height correspondence scatterplot and saves it in output_dir/correlation_figures
-    make_height_corr_plots(output_dir = output_dir,
-                     predicted_trees_filepath = predicted_trees_filepath)
-
+                   virtual_plot_size = 30, # The height and width of contiguous square grid cells laid over the field plot for computing area-based statistics
+                   additional_overstory_comparison = TRUE) # In addition to comparing against *all* observed trees, should we compare against *overstory trees only*. If so, the observed trees stem map file needs to have an attribute "under_neighbor" previously assigned by the script scripts/ground_stem_map_assign_under_neighbor.R
+  
+  # This function combines the individual tree detection and area-based accuracy statistics and saves in output_dir/tree_detection_evals
+  combine_stats(tmp_dir = tmp_dir,
+                output_dir = output_dir,
+                predicted_trees_filepath = predicted_trees_filepath)
+  
+  # This function compares the heights of paired predicted and observed trees and makes a height correspondence scatterplot and saves it in output_dir/correlation_figures
+  make_height_corr_plots(output_dir = output_dir,
+                         predicted_trees_filepath = predicted_trees_filepath)
+  
 }
 
 
 predicted_ttop_files = list.files(predicted_trees_path, full.names = TRUE, pattern = "gpkg$")
 
 plan(multisession)
-furrr_options(scheduling = Inf)
+furrr_options(chunk_size = 10)
 future_walk(predicted_ttop_files, eval_one_predicted_set)
 
